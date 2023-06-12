7c064fd84d3323dcb1c78fca9eb6279f5cb0a8c1
William Gillett <wmgillett@gmail.com>
2023-06-12 05:10:13 -0400

    reorganized classes, added more tests, cleaned up log messages

--- tests/test_transcribe_yt.py
+++ tests/test_transcribe_yt.py
@@ -3,18 +3,34 @@
 import pytest
 #from unittest.mock import patch, MagicMock
 from unittest.mock import Mock, patch, MagicMock
-from transcribe_yt import myTranscriber, myTextSplitter
+from transcribe_yt import myTranscriber, myTextSplitter, getMetadata, downloadSource
 
+#@patch('transcribe_yt.myTranscriber.save_to_mp3')
+#@patch('transcribe_yt.myTranscriber.save_to_mp3', return_value='mock_value')
+#def test_save_to_mp3(mock_save_to_mp3):
+    #transcriber = myTranscriber('tiny.en')  # assuming 'model_size' is a valid argument
+    # Now when you call transcriber.save_to_mp3, it won't execute the real method, but will return 'mock_value' instead.
+#def test_transcribe_youtube_video2():
+#    transcriber = myTranscriber('tiny.en')  # assuming 'model_size' is a valid argument
+#    transcriber.save_to_mp3 = MagicMock(return_value='mock_value')  # replace the instance method with a mock
+    # call your function and check the results
+#    result = transcriber.transcribe_youtube_video('https://youtu.be/dQw4w9WgXcQ', 'model') # Replace the URL and model with actual value
+    # Assert the result
+#    assert result is not None # change this to your actual assertion
 @pytest.fixture
 @patch('transcribe_yt.whisper.load_model')
 def transcriber(mock_load_model):
     mock_load_model.return_value = Mock()
-    return myTranscriber('base.en')
-print("Running tests...")
-
+    return myTranscriber('tiny.en')
 @pytest.fixture
 def splitter():
     return myTextSplitter({'text': "Hello world! I am a transcriber."}, 5)
+@pytest.fixture
+def get_metadata():
+    return getMetadata()
+@pytest.fixture
+def download_source():
+    return downloadSource()
 
 @patch('transcribe_yt.whisper.load_model')
 def test_transcriber_initialization(mock_load_model):
@@ -35,7 +51,7 @@ def test_transcriber_initialization(mock_load_model):
 
 # test get_channel_list
 @patch('transcribe_yt.youtube_dl.YoutubeDL')
-def test_get_channel_list(mock_youtube_dl, transcriber):
+def test_get_channel_list(mock_youtube_dl, transcriber, get_metadata):
     mock_extractor = Mock()
     #mock_extractor.extract_info.return_value = {'entries': ['url1', 'url2']}
     mock_extractor.extract_info.return_value = {'entries': [{'url': 'url1'}, {'url': 'url2'}]}
@@ -44,7 +60,7 @@ def test_get_channel_list(mock_youtube_dl, transcriber):
     # replace with a real channel URL for the test
     channel_url = "https://www.youtube.com/@RickAstleyYT/videos" 
     channel_name = "test_channel"
-    info_dict = transcriber.get_channel_list(channel_url, channel_name)
+    info_dict = get_metadata.get_channel_list(channel_url, channel_name)
 
     assert info_dict is not None
     assert 'entries' in info_dict
@@ -76,10 +92,12 @@ def test_transcribe_youtube_video(transcriber):
     video_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
     result = transcriber.transcribe_youtube_video(video_url)
     assert result is not None
-
+    # buggy test - need to fix
+    #result_invalid = transcriber.transcribe_youtube_video('https://youtu.be/INVALIDdQw4w9WgXcQ') 
+    #assert result_invalid is None # 
 
 @patch('transcribe_yt.youtube_dl.YoutubeDL')
-def test_get_video_metadata(mock_youtube_dl, transcriber):
+def test_get_video_metadata(mock_youtube_dl, transcriber, get_metadata):
     mock_extractor = Mock()
     mock_extractor.extract_info.return_value = {
         'url': 'test_url', 'title': 'test_title', 'description': 'test_description',
@@ -90,7 +108,7 @@ def test_get_video_metadata(mock_youtube_dl, transcriber):
 
     # replace with a real video URL for the test
     video_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-    metadata = transcriber.get_video_metadata(video_url)
+    metadata = get_metadata.get_video_metadata(video_url)
 
     assert metadata is not None
     assert 'url' in metadata
@@ -111,3 +129,13 @@ def test_split_into_sentences(splitter):
     assert len(sentences) == 2
     assert sentences[0] == "Hello world!"
     assert sentences[1] == "I am a transcriber."
+
+# test downloadAudio
+@patch('transcribe_yt.youtube_dl.YoutubeDL')
+def test_download_audio(mock_youtube_dl, download_source):
+    video_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    filename = download_source.download_audio(video_url)
+
+    assert filename is not None
+    #assert os.path.isfile(filename)
+    #os.remove(filename)
